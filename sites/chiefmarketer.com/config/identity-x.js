d2a6b18f983e1251c147423453e00f55b0fb335b
Jessica Wade <jessica.wade@parameter1.com>
2022-06-09 22:06:54 -0500

    Update identityx appID

--- sites/chiefmarketer.com/config/identity-x.js
+++ sites/chiefmarketer.com/config/identity-x.js
@@ -1,7 +1,7 @@
 const configureIdentityX = require('@access-intelligence/package-global/config/identity-x');
 
 module.exports = configureIdentityX({
-  appId: '62a20aad39347c4830862983',
+  appId: '62a20b5a6ca6061b4107c28d',
   requiredServerFields: [
     'givenName',
     'familyName',

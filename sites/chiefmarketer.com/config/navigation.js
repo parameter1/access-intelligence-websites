4b054d727a31c3b748a7249bfad9b4833b49897d
Jessica Wade <jessica.wade@parameter1.com>
2022-06-09 22:06:36 -0500

    Add navigation Topics

--- sites/chiefmarketer.com/config/navigation.js
+++ sites/chiefmarketer.com/config/navigation.js
@@ -2,12 +2,11 @@ const privacyPolicy = require('./privacy-policy');
 const subscribe = require('./subscribe');
 
 const topics = [
-  { href: '/publishers', label: 'Publishers' },
-  // { href: '/regulation', label: 'Regulation' },
-  // { href: '/distribution', label: 'Distribution' },
-  // { href: '/technology', label: 'Technology' },
-  // { href: '/people', label: 'People' },
-  // { href: '/associations', label: 'Associations' },
+  { href: '/consumer-marketing', label: 'Consumer Marketing' },
+  { href: '/b2b-marketing', label: 'B2B Marketing' },
+  { href: '/martech', label: 'Martech' },
+  { href: '/events-honors', label: 'Events & Honors' },
+  { href: '/directories', label: 'Directories' },
 ];
 
 const resources = [];
@@ -58,6 +57,44 @@ module.exports = {
   tertiary: {
     items: [],
   },
+  // contexts: [
+  //   {
+  //     when: ['/consumer-marketing'],
+  //     primary: { topics },
+  //     secondary: {
+  //       items: [
+  //         { href: '/consumer-marketing/acquisition', label: 'Acquisition' },
+  //         { href: '/consumer-marketing/retention', label: 'Retention' },
+  //         { href: '/consumer-marketing/data-analytics', label: 'Data Analytics' },
+  //       ],
+  //     },
+  //   },
+  //   {
+  //     when: ['/b2b-marketing'],
+  //     primary: { topics },
+  //     secondary: {
+  //       items: [
+  //         { href: '/b2b-marketing/aquisition', label: 'Aquisition' },
+  //         { href: '/b2b-marketing/retention', label: 'Retention' },
+  //         { href: '/b2b-marketing/data-analytics', label: 'Data Analytics' },
+  //       ],
+  //     },
+  //   },
+  //   {
+  //     when: ['/martech'],
+  //     primary: { topics },
+  //     secondary: {
+  //       items: [
+  //         { href: '/martech/cmo-corner', label: 'CMO Corner' },
+  //         { href: '/martech/technology', label: 'Technology' },
+  //         { href: '/martech/roi-metrics', label: 'ROI Metrics' },
+  //         { href: '/martech/agencies', label: 'Agencies' },
+  //         { href: '/martech/research', label: 'Research' },
+  //       ],
+  //     },
+  //   },
+  // ],
+
   footer: {
     items: [
       privacyPolicy,
